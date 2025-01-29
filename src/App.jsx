b2fa1f8c0247ab42ac6dd9e50056dd667b5b57f6
Raul772 <raulvictor772@gmail.com>
2025-01-29 15:55:16 -0300

    feat: Navbar

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<h1>Página não existe</h1>} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
