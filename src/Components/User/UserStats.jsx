997a355302cfa11f0cfb76c72b97d09fa869f262
Raul Victor <31050006+Raul772@users.noreply.github.com>
2025-02-02 18:54:44 -0300

    feat: add stats page with lazy module import

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,11 +1,37 @@
+import { lazy, Suspense, useEffect } from "react";
+import { STATS_GET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import { Head } from "../Helper/Head";
+import { Loading } from "../Helper/Loading";
+
+const UserStatsGraphs = lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+  const token = window.localStorage.getItem("token");
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET(token);
+
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error>{error}</Error>;
+  if (data)
+    return (
+      <Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </Suspense>
+    );
+
+  return null;
 };
 
 export default UserStats;
